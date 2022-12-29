bb9e0005ded2ff061650268a823e4145bceff219
Dahan Israel David <dahani3@medtronic.com>
2022-12-29 10:01:50 +0200

    fix layout

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import Col from 'react-bootstrap/Col';
 
 const Article = (props) => {
     const {title, urlToImage, description, url, source} = props.data
   return (
-    <Card style={{ width: "18rem" }}>
+    <Col md={3} >
+    <Card >
       <Card.Img variant="top" src={urlToImage} />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
@@ -15,6 +17,7 @@ const Article = (props) => {
         <Button variant="primary" href={url} target="_blank">News from <b>{source.name}</b></Button>
       </Card.Body>
     </Card>
+    </Col>
   );
 };
 
--- src/components/ArticlesFromService.js
+++ src/components/ArticlesFromService.js
@@ -1,14 +1,24 @@
 import Article from './Article';
 import useDataFetching from '../services/useDataFetching'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Pagination from 'react-bootstrap/Pagination';
+
 // import axios from 'axios';
 const ArticlesFromService = () => {
     const apiKey = '6bd9cc4990674c61b79f5c98ea935eb9';
     let q = 'tesla';
     let from = '2022-11-29';
     let sortBy = 'publishedAt';
-    const url = `https://newsapi.org/v2/everything?q=${q}&from=${from}&sortBy${sortBy}=&apiKey=${apiKey}`;
+    let pageSize = 4;
+    let page = 3;
+    const url = `https://newsapi.org/v2/everything?q=${q}&from=${from}&sortBy${sortBy}=&pageSize=${pageSize}&page=${page}&apiKey=${apiKey}`;
     const { data, loading, error } = useDataFetching(url);
 
+    // const handleClickPagination = (e) => {
+    //     console.log(e.target.innerHTML);
+    
+    // }
     if (loading) {
         return <p>Loading...</p>;
     }
@@ -16,8 +26,37 @@ const ArticlesFromService = () => {
     if (error) {
         return <p>Error: {error.message}</p>;
     }
+    return (
+        <Container>
+            <Container>
+                <Row>
+                    {data.articles.map((article, key) => <Article data={article} key={key}/>)}
+                </Row>
+            </Container>
+            {/* <Container>
+                <Row>
+                    <Pagination>
+                        <Pagination.First />
+                        <Pagination.Prev />
+                        <Pagination.Item onClick={handleClickPagination}>{1}</Pagination.Item>
+                        <Pagination.Ellipsis />
 
-    return <p>Data: {data.articles.map((article) => <Article data={article} />)}</p>;
+                        <Pagination.Item onClick={handleClickPagination}>{10}</Pagination.Item>
+                        <Pagination.Item onClick={handleClickPagination}>{11}</Pagination.Item>
+                        <Pagination.Item onClick={handleClickPagination} active>{12}</Pagination.Item>
+                        <Pagination.Item onClick={handleClickPagination} >{13}</Pagination.Item>
+                        <Pagination.Item onClick={handleClickPagination}>{14}</Pagination.Item>
+
+                        <Pagination.Ellipsis />
+                        <Pagination.Item>{20}</Pagination.Item>
+                        <Pagination.Next />
+                        <Pagination.Last />
+                    </Pagination>
+                </Row>
+            </Container> */}
+
+        </Container>
+    )
 }
 
 export default ArticlesFromService;
